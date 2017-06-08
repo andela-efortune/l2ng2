84019f9d94c8b74a7316c41b93cb407f93fdd630
Fortune Ekeruo <legalmody@gmail.com>
2017-06-08 14:16:40 +0100

    Styling components

--- app/shared/layout/header-component.ts
+++ app/shared/layout/header-component.ts
@@ -2,7 +2,8 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'layout-header',
-  templateUrl: 'app/shared/layout/header-component.html'
+  templateUrl: 'app/shared/layout/header-component.html',
+  styleUrls: ['app/shared/layout/header-component.css']
 })
 export class HeaderComponent {
   constructor() {}
